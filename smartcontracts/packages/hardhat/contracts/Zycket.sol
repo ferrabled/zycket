bba846a1755c7b30a525d148db57cd58d8bdeb0b
Glaicon <gjpeixer@gmail.com>
2024-03-16 11:09:19 +0000

    Update smart contract

---
+++ smartcontracts/packages/hardhat/contracts/Zycket.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Zycket is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("Zycket", "ZKT")
+        Ownable()
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
--- smartcontracts/packages/hardhat/test/YourContract.ts
+++ smartcontracts/packages/hardhat/test/YourContract.ts
@@ -1,28 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { YourContract } from "../typechain-types";
+import { Zycket } from "../typechain-types";
 
-describe("YourContract", function () {
+describe("Zycket", function () {
   // We define a fixture to reuse the same setup in every test.
 
-  let yourContract: YourContract;
+  let yourContract: Zycket;
   before(async () => {
     const [owner] = await ethers.getSigners();
-    const yourContractFactory = await ethers.getContractFactory("YourContract");
-    yourContract = (await yourContractFactory.deploy(owner.address)) as YourContract;
+    const yourContractFactory = await ethers.getContractFactory("Zycket");
+    yourContract = (await yourContractFactory.connect(owner).deploy(owner.address)) as Zycket;
     await yourContract.waitForDeployment();
   });
 
   describe("Deployment", function () {
     it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
+      const [owner, bob] = await ethers.getSigners();
+      expect(await yourContract.balanceOf(owner.address)).to.equal(0);
+      await yourContract.safeMint(owner.address, 1)
+      expect(await yourContract.balanceOf(owner.address)).to.equal(1);
     });
 
-    it("Should allow setting a new message", async function () {
-      const newGreeting = "Learn Scaffold-ETH 2! :)";
-
-      await yourContract.setGreeting(newGreeting);
-      expect(await yourContract.greeting()).to.equal(newGreeting);
-    });
   });
 });
