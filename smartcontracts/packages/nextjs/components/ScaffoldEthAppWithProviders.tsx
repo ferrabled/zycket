9c2bfe7b49fb66f72f05c49da90ef9abf2e9881b
Julio M Cruz <julio.cruz@eb-ms.net>
2024-03-15 22:50:29 -0400

    Smart contracts Boilerplate

---
+++ smartcontracts/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect } from "react";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { Toaster } from "react-hot-toast";
+import { Footer } from "~~/components/Footer";
+import { Header } from "~~/components/Header";
+import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
+import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
+import { useDarkMode } from "~~/hooks/scaffold-eth/useDarkMode";
+import scaffoldConfig from "~~/scaffold.config";
+import { useGlobalState } from "~~/services/store/store";
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  const price = useNativeCurrencyPrice();
+  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
+
+  useEffect(() => {
+    if (price > 0) {
+      setNativeCurrencyPrice(price);
+    }
+  }, [setNativeCurrencyPrice, price]);
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="relative flex flex-col flex-1">{children}</main>
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+};
+
+export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
+  const { isDarkMode } = useDarkMode();
+
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: scaffoldConfig.dynamicEnvId,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+      theme={isDarkMode ? "dark" : "light"}
+    >
+      <ProgressBar />
+      <DynamicWagmiConnector>
+        <ScaffoldEthApp>{children}</ScaffoldEthApp>
+      </DynamicWagmiConnector>
+    </DynamicContextProvider>
+  );
+};
