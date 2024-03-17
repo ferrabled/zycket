c42f781597bd44f526a4574ef82779cb3d315f75
Fernando Rabasco Ledesma <frabascol@gmail.com>
2024-03-17 08:24:01 +0100

    Dashboard finish

--- zycketApp/components/dashboard.tsx
+++ zycketApp/components/dashboard.tsx
@@ -3,9 +3,10 @@ import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/com
 import { Badge } from "@/components/ui/badge"
 import { useState } from "react";
 import SellModal from "./marketplace/sellModal";
+import { useRouter } from 'next/navigation'
 
 export default function DashboardComponent() {
-  
+  const router = useRouter()
   const [show, setShow] = useState(false)
 
 
@@ -14,17 +15,18 @@ export default function DashboardComponent() {
       setShow(true)
       console.log('purchased');
     }
-  
+    
+
     return (
       <Card className="w-full">
         <CardHeader>
           <CardTitle>ETHLondon</CardTitle>
-          <CardDescription>Emirates Stadium, London</CardDescription>
+          <CardDescription>Bringing developers onchain to build for the future of the internet.</CardDescription>
+          <CardDescription>91 Brick Ln, London E1 6QR.</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p className="text-sm text-gray-600">93 Feet East</p>
-          <Button onClick={sellItem}>Sell</Button>
-          <Button onClick={()=> console.log("JA")}>Entrance Verify</Button>
+        <CardContent className="flex flex-row gap-5">
+          <Button variant="destructive" onClick={sellItem}>Marketplace Sell</Button>
+          <Button onClick={() => router.push('/verification')}>Entrance Verify</Button>
         </CardContent>
       </Card>
     )
@@ -42,44 +44,36 @@ export default function DashboardComponent() {
         </div>
         <section className="space-y-8">
           <div>
-            <h3 className="text-lg font-semibold text-gray-600 mb-4">Thursday, March 14</h3>
+            <h3 className="text-lg font-semibold text-gray-600 mb-4">Sunday, March 17</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               <Event />
               <SellModal show={show} setShow={setShow}/>
               <Card className="w-full">
                 <CardHeader>
-                  <CardTitle>Manchester United vs. Liverpool</CardTitle>
-                  <CardDescription>Old Trafford, Manchester</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-sm text-gray-600">Rich Mix</p>
-                  <Button variant="ghost">Attendee</Button>
-                </CardContent>
-              </Card>
-              <Card className="w-full">
-                <CardHeader>
                   <CardTitle>Manchester City vs. Tottenham</CardTitle>
-                  <CardDescription>2b Worship St</CardDescription>
+                  <CardDescription>A match that you cannot miss!</CardDescription>
+                  <CardDescription>Manchester City Stadium</CardDescription>
                 </CardHeader>
-                <CardContent>
-                  <Button variant="ghost">Attendee</Button>
-                  <Badge variant="secondary">+118</Badge>
+                <CardContent className="flex flex-row gap-5">
+                  <Button variant="destructive">Marketplace Sell</Button>
+                  <Button onClick={() => router.push('/verification')}>Entrance Verify</Button>
                 </CardContent>
               </Card>
             </div>
           </div>
           <div>
-            <h3 className="text-lg font-semibold text-gray-600 mb-4">Wednesday, March 13</h3>
+            <h3 className="text-lg font-semibold text-gray-600 mb-4">Monday, March 18</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               <Card className="w-full">
                 <CardHeader>
                   <CardTitle>Real Madrid vs. Barcelona</CardTitle>
+                  <CardDescription>Best Match of the season!</CardDescription>
                   <CardDescription>Bernabeu Stadium, Madrid</CardDescription>
                 </CardHeader>
-                <CardContent>
-                  <p className="text-sm text-gray-600">10 York Rd</p>
-                  <Button variant="ghost">Attendee</Button>
-                </CardContent>
+                <CardContent className="flex flex-row gap-5">
+          <Button variant="destructive">Marketplace Sell</Button>
+          <Button onClick={() => router.push('/verification')}>Entrance Verify</Button>
+        </CardContent>
               </Card>
             </div>
           </div>
